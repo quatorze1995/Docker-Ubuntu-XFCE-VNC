5af7f67c6a6d2817776cad1d06a45ad03afa89f0
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-03-22 13:03:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,13 +23,45 @@ echo -e "$VNC_PASSWORD\n$VNC_PASSWORD\n" | vncpasswd -f > /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd
 echo "VNC password file created and permissions secured."
 
+if [ -z "$P2P_EMAIL" ]; then
+  echo "P2P_EMAIL is not set or is blank. Skipping creation of the Peer2Profit configuration file."
+else
+  FLAG_FILE="/home/root/.config/org.peer2profit.setup_done"
+
+  if [ -f "$FLAG_FILE" ]; then
+    echo "Peer2Profit configuration file has already been created. Skipping."
+  else
+    INSTALL_ID2=$(uuidgen)
+    CONFIG_FILE="/home/root/.config/org.peer2profit.peer2profit.ini"
+    CONFIG_DIR=$(dirname "$CONFIG_FILE")
+    mkdir -p "$CONFIG_DIR"
+    cat <<EOF > "$CONFIG_FILE"
+[General]
+Username=$P2P_EMAIL
+hideToTrayMsg=true
+installid2=$INSTALL_ID2
+locale=en_US
+EOF
+
+    chown -R "root:root" "$CONFIG_DIR"
+
+    # Create the flag file to mark the script as completed
+    touch "$FLAG_FILE"
+    echo "Peer2Profit configuration file created successfully at $CONFIG_FILE."
+  fi
+fi
+
 cat << EOF > /root/.vnc/xstartup
 #!/bin/sh
 xrdb \$HOME/.Xresources
 startxfce4 &
 EOF
+
 chmod +x /root/.vnc/xstartup
 echo "VNC xstartup script created and made executable."
+
+echo "Starting SSH service..."
+service ssh restart
 
 echo "Stopping any existing VNC server processes..."
 pkill -f "Xvnc" || echo "No existing VNC processes found."
