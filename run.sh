2bf667839f81236a65c8438cdde108fdf298f97d
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-03-25 20:49:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run -d -p 5901:5901 -p 6080:6080 -p 22222:22222 \
+docker run -d -p 5901:5901 -p 5902:5902 -p 5903:5903 -p 6080:6080 -p 22222:22222 \
   -e VNC_PASSWORD=password \
   -e VNC_RESOLUTION=1600x900 \
   -e P2P_EMAIL=email@example.com \
