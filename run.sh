18420d5030dda279e0818b340b2d68933f75e99e
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-04-16 21:53:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ docker run -d -p 5901:5901 -p 5902:5902 -p 5903:5903 -p 6080:6080 -p 22222:22222
   -e VNC_PASSWORD=password \
   -e VNC_RESOLUTION=1600x900 \
   -e P2P_EMAIL=email@example.com \
+  -v /etc/_docker/ubuntu-xfce-vnc:/root/.local/share
   ubuntu-xfce-vnc
