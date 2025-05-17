4475ac38aa678c4287840982396d69a571a0dda0
quatorze1995 <80513295+quatorze1995@users.noreply.github.com>
2025-05-17 04:37:34 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-docker run -d -p 5901:5901 -p 6080:6080 -p 22222:22222 \
+docker run -d -p 5901:5901 -p 6080:6080 \
   -e VNC_PASSWORD=password \
-  -e VNC_RESOLUTION=1600x900 \
+  -e VNC_RESOLUTION=1024x768 \
   -e P2P_EMAIL=email@example.com \
   -e WIPTER_EMAIL=email@example.com \
   -e WIPTER_PASSWORD=password \
