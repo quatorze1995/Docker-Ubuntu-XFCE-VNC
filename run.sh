79b2017c11ceed371e6af8fbfdf47e2fd711948f
quatorze1995 <80513295+quatorze1995@users.noreply.github.com>
2025-05-17 04:38:27 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 docker run -d -p 5901:5901 -p 6080:6080 \
   -e VNC_PASSWORD=password \
   -e VNC_RESOLUTION=1024x768 \
-  -e P2P_EMAIL=email@example.com \
-  -e WIPTER_EMAIL=email@example.com \
-  -e WIPTER_PASSWORD=password \
-  -v /etc/_docker/ubuntu-xfce-vnc:/root/.local/share \
   ubuntu-xfce-vnc
